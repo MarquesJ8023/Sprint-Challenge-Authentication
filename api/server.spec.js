36102b673732f7297efcf172e2e57165de7ef458
Marques <marques.johnson1992@gmail.com>
2019-09-20 16:28:59 -0400

    last commit

--- api/server.spec.js
+++ api/server.spec.js
@@ -3,34 +3,10 @@ const request = require('supertest');
 const server = require('./server.js');
 
 describe('server', () => {
-    describe('GET /auth', () => {
+    describe('POST /api/auth', () => {
         it('returns 201 saved', () => {
           return request(server)
-          .get('/auth')
-          .then(res => {
-                expect(res.status).toBe(201);
-            });
-        });
-    });
-});
-
-describe('server', () => {
-    describe('GET /', () => {
-        it('returns 500 error', () => {
-          return request(server)
-          .get('/jokes')
-          .then(res => {
-                expect(res.status).toBe(500);
-            });
-        });
-    });
-});
-
-describe('server', () => {
-    describe('POST /jokes', () => {
-        it('returns 200 token', () => {
-          return request(server)
-          .get('/jokes')
+          .get('/api/auth')
           .then(res => {
                 expect(res.status).toBe(200);
             });
@@ -39,10 +15,34 @@ describe('server', () => {
 });
 
 describe('server', () => {
-    describe('POST /jokes', () => {
+    describe('POST /api/jokes', () => {
+        it('returns 500 error', () => {
+          return request(server)
+          .get('/api/jokes')
+          .then(res => {
+                expect(res.status).toBe(500);
+            });
+        });
+    });
+});
+
+describe('server', () => {
+    describe('POST /api/jokes', () => {
+        it('returns token', () => {
+          return request(server)
+          .get('/api/jokes')
+          .then(res => {
+                expect(res.status).toBe(200);
+            });
+        });
+    });
+});
+
+describe('server', () => {
+    describe('POST /api/jokes', () => {
         it('returns 401 invalid credentials', () => {
           return request(server)
-          .get('/jokes')
+          .get('/api/jokes')
           .then(res => {
                 expect(res.status).toBe(401);
             });
